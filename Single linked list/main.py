41c82fec9f130de4a55b20a7c80d0fb6a68ec049
healthcanadacosmetics <ricardoamsrams@gmail.com>
2025-04-06 15:02:44 -0400

    Add prepend operation to linked list and node class

---
+++ Single linked list/main.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head=None
+
+    def prepend(self,data):
+        '''
+        This method append a node at the beginning of the linked list
+        Args:
+            data: Var that contains the data of each node
+        '''
+        if not self.head:
+            new_node=Node(data)
+            self.head=new_node
+        else:
+            new_node=Node(data)
+            new_node.next=self.head
+            self.head=new_node
+
+    def printNodes(self):
+        cur=self.head
+        while cur:
+            print(cur.data)
+            cur=cur.next
+
+if __name__=="__main__":
+    singleLinkedListObj=SingleLinkedList()
+    singleLinkedListObj.prepend(10)
+    singleLinkedListObj.prepend(34)
+    singleLinkedListObj.prepend(15)
+    singleLinkedListObj.prepend(6)
+    singleLinkedListObj.printNodes()
+    
+    
+    
